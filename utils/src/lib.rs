6f381d9a9a3ad6a113ba9f5f3f0701097b839b38
Adam Rodger <adam.rodger@gmail.com>
2025-12-25 12:03:03 +0000

    refactor: Move input files to hierarchical layout

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -2,7 +2,7 @@
 /// from the inputs directory in the workspace root.
 pub fn load_event_input(event: u32, quest: u32, part: u32) -> String {
     let workspace = env!("CARGO_MANIFEST_DIR");
-    let path = format!("{workspace}/../inputs/everybody_codes_e{event}_q{quest:02}_p{part}.txt");
+    let path = format!("{workspace}/../inputs/e{event}/q{quest:02}/p{part}.txt");
 
     let input = std::fs::read_to_string(&path)
         .unwrap_or_else(|_| panic!("Failed to read input file at {path}"));
